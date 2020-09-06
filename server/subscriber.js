3546cc15a8b10e2e278b1c81fc34e2c04565e823
Horlarmeday <ajaomahmud@gmail.com>
2020-09-06 09:59:45 +0100

    changed to amazon ses

--- server/subscriber.js
+++ server/subscriber.js
@@ -4,12 +4,12 @@ import amqplib from 'amqplib';
 
 // Setup nodemailer transport
 const transport = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'smtpEndpoint',
     port: 465,
     secure: true,
     auth: {
-        user: process.env.USER,
-        pass: process.env.PASS
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 });
 
@@ -25,7 +25,7 @@ const subscriber = async () => {
         const ch = await conn.createChannel();
 
         // Ensure queue for messages and Ensure that the queue is not deleted when server restarts
-        await ch.assertQueue(q, { durable: true });
+        await ch.assertQueue(q, {durable: true});
 
         // Only request 1 unacked message from queue
         // This value indicates how many messages we want to process in parallel
@@ -34,18 +34,35 @@ const subscriber = async () => {
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
 
         // Set up callback to handle messages received from the queue
-        ch.consume(q, async function(msg) {
+        ch.consume(q, async function (msg) {
             console.log(" [x] Received %s", msg.content.toString());
 
             const qm = JSON.parse(msg.content.toString());
 
-            const { name, email, subject, message } = qm;
+            const {name, email, subject, message} = qm;
+
+            // The message tags to apply to the email.
+            const tag0 = "key0=value0";
+            const tag1 = "key1=value1";
 
             const mailOption = {
                 from: process.env.USER,
                 to: process.env.RECEIVER,
                 subject: 'PERSONAL PORTFOLIO',
-                text: `Email: ${email}, name: ${name}, subject: ${subject}, message: ${message}`
+                text: `Email: ${email}, name: ${name}, subject: ${subject}, message: ${message}`,
+                html: `<html lang="">
+                        <head><title>${subject}</title></head>
+                        <body>
+                            <h3>${name}</h3>
+                            <p>Email: ${email}</p>
+                            <p><strong>Message: </strong>${message}</p>
+                        </body>                    
+                    </html>`,
+                // Custom headers for message tags.
+                headers: {
+                    'X-SES-MESSAGE-TAGS': tag0,
+                    'X-SES-MESSAGE-TAGS': tag1
+                }
             };
 
             // Send the message using the previously set up Nodemailer transport
